be7902f9e42e77793a55590f185454e68aaf6f10
zl-neoy <ywoo12121@gmail.com>
2021-08-27 18:02:01 +0900

    Nomard #2.0 듣다말음

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import AppRouter from './Router';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return <AppRouter isLoggedIn = {isLoggedIn}/>;
+  return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
